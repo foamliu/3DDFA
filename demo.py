8252cccbded94e7a57727b468728534faf27f511
Yang Liu <liuyang12@focusmedia.cn>
2020-01-02 15:46:17 +0800

    update

--- demo.py
+++ demo.py
@@ -1,16 +1,17 @@
 import cv2 as cv
+import dlib
 import numpy as np
 import scipy.io as sio
 import torch
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
-import dlib
+
 from config import device
 from misc import ensure_folder
 from utils.ddfa import ToTensorGjz, NormalizeGjz
 from utils.estimate_pose import parse_pose
-from utils.inference import predict_68pts, parse_roi_box_from_bbox, predict_dense, dump_to_ply, get_suffix, get_colors, \
-    write_obj_with_colors, crop_img
+from utils.inference import predict_68pts, predict_dense, dump_to_ply, get_suffix, get_colors, \
+    write_obj_with_colors, crop_img, parse_roi_box_from_landmark
 
 if __name__ == '__main__':
     checkpoint = 'BEST_checkpoint.tar'
@@ -23,16 +24,24 @@ if __name__ == '__main__':
     model.eval()
 
     face_detector = dlib.get_frontal_face_detector()
+    dlib_landmark_model = 'models/shape_predictor_68_face_landmarks.dat'
+    face_regressor = dlib.shape_predictor(dlib_landmark_model)
+
     transform = transforms.Compose([ToTensorGjz(), NormalizeGjz(mean=127.5, std=128)])
 
     filename = 'images/0008.png'
     img_ori = cv.imread(filename)
     rects = face_detector(img_ori, 1)
     rect = rects[0]
-    bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
-    print('bbox: ' + str(bbox))
-    roi_box = parse_roi_box_from_bbox(bbox)
-    print('roi_box: ' + str(roi_box))
+    # bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
+    # print('bbox: ' + str(bbox))
+    # roi_box = parse_roi_box_from_bbox(bbox)
+    # print('roi_box: ' + str(roi_box))
+
+    # - use landmark for cropping
+    pts = face_regressor(img_ori, rect).parts()
+    pts = np.array([[pt.x, pt.y] for pt in pts]).T
+    roi_box = parse_roi_box_from_landmark(pts)
 
     img = crop_img(img_ori, roi_box)
 
