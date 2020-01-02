836c283ec9c816a611f64b547906d8547c100e75
Yang Liu <liuyang12@focusmedia.cn>
2020-01-02 15:58:35 +0800

    update

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ from misc import ensure_folder
 from utils.ddfa import ToTensorGjz, NormalizeGjz
 from utils.estimate_pose import parse_pose
 from utils.inference import predict_68pts, predict_dense, dump_to_ply, get_suffix, get_colors, \
-    write_obj_with_colors, crop_img, parse_roi_box_from_landmark
+    write_obj_with_colors, crop_img, parse_roi_box_from_landmark, parse_roi_box_from_bbox
 
 if __name__ == '__main__':
     checkpoint = 'BEST_checkpoint.tar'
@@ -33,15 +33,15 @@ if __name__ == '__main__':
     img_ori = cv.imread(filename)
     rects = face_detector(img_ori, 1)
     rect = rects[0]
-    # bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
+    bbox = [rect.left(), rect.top(), rect.right(), rect.bottom()]
     # print('bbox: ' + str(bbox))
-    # roi_box = parse_roi_box_from_bbox(bbox)
+    roi_box = parse_roi_box_from_bbox(bbox)
     # print('roi_box: ' + str(roi_box))
 
     # - use landmark for cropping
-    pts = face_regressor(img_ori, rect).parts()
-    pts = np.array([[pt.x, pt.y] for pt in pts]).T
-    roi_box = parse_roi_box_from_landmark(pts)
+    # pts = face_regressor(img_ori, rect).parts()
+    # pts = np.array([[pt.x, pt.y] for pt in pts]).T
+    # roi_box = parse_roi_box_from_landmark(pts)
 
     img = crop_img(img_ori, roi_box)
 
